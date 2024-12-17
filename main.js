77a02f9862818daf8bd174038bccdf87e031c84e
AshishIT13 <ay87896@gmail.com>
2024-12-17 14:19:21 +0530

    Added work

---
+++ main.js
@@ -0,0 +1,19 @@
+document.getElementById("button").addEventListener("click",(e)=>{
+    e.preventDefault();
+    const name=document.getElementById("name").value.trim();
+    const phoneNumber=document.getElementById("phone_number").value.trim();
+    const email=document.getElementById("email").value.trim();
+    const division=document.getElementById("division").value.trim();
+    const city=document.getElementById("city").value.trim();
+    if(!name||!phoneNumber||!email||!division||!city){
+        alert("Please fill out all the fields!");
+        return;
+    }
+    console.log(`Name:${name}`);
+    console.log(`Phone Number:${phoneNumber}`);
+    console.log(`Email:${email}`);
+    console.log(`Division:${division}`);
+    console.log(`City:${city}`);
+    document.getElementById("success").textContent="Successfully Submitted";
+    document.querySelector("form").reset();
+});
